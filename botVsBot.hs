aef1ce51d47e61e6c80da84df23ae48dd4af7cfe
CaptainLeviathan <levmckinney@gmail.com>
2018-05-30 23:52:11 -0700

    botVsBot and updated git ignore

--- botVsBot.hs
+++ botVsBot.hs
@@ -13,29 +13,32 @@ main = do gen <- getStdGen
 
 playGame :: (Integer, Integer, Integer) -> Integer -> IO ()
 playGame winloss rounds = do if rounds > 0 
-                             then do gen <- newStdGen
-                                     let resultString (b,r,d) = ("bot has won: " ++ show b ++ " Random has won: " ++ show r ++ " Draws: " ++ show d ++ " gen was: " ++ show gen)
+                             then do stdget <- getStdGen
+                                     gen <- newStdGen
+                                     let resultString (b,r,d) = ("bot has won: " ++ show b ++ " Random has won: " ++ show r ++ " Draws: " ++ show d ++ " gen was: " ++ show stdget)
                                      putStrLn $ resultString winloss
                                      playGame (playFullRound winloss gen) (rounds - 1)
                              else return ()
 
 playFullRound :: (Integer, Integer, Integer) -> StdGen -> (Integer, Integer, Integer)
-playFullRound (iBotWins, iDraws, iRandWins) gen = (iBotWins + botXWins + botOWins, iRandWins + randXwins + randOwins, iDraws + draws)
+playFullRound (iBotWins, iRandWins, iDraws) gen = (iBotWins + botXWins + botOWins, iRandWins + randXWins + randOWins, iDraws + draws + someThingWrong)
     where botXWins = if xBot == X then 1 else 0
           botOWins = if xRand == O then 1 else 0
-          randXwins = if xRand == X then 1 else 0
-          randOwins = if xBot == O then 1 else 0
-          draws = if xBot == Empty then 1 else 0 + if xRand == Empty then 1 else 0
+          randXWins = if xRand == X then 1 else 0
+          randOWins = if xBot == O then 1 else 0
+          draws = (if xBot == Empty then 1 else 0) + (if xRand == Empty then 1 else 0)
+          someThingWrong-- just for testing
+             | botXWins + botOWins + randXWins + randOWins + draws == 2 = 0
+             | otherwise = error $ "why is this happening xBot: " ++ show xBot ++ " xRand: " ++ show xRand ++ " botXwins: " ++ show botXWins ++ " botOWins: " ++ show botOWins ++ " randXWins " ++ show randXWins ++ " randOwins: " ++ show randOWins ++ " draws: " ++ show draws 
           xBot = playBotGameBotX gen
           xRand = playBotGameRandX gen
 
 
 
 
-addMoves moves = fmap (\m -> moves ++ [m])
-
-
 botDepth = 3
+
+addMoves moves = fmap (\m -> moves ++ [m])
 
 playBotGameBotX :: StdGen -> XO
 playBotGameBotX startGen = nextRoud startGen []
